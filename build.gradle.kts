dce00608dc23ce510a1112a00cbfa5c0f83cd205
Christian <r0bby@gmx.net>
2021-08-17 19:52:51 +0200

    Bump gradle plugin version 0.1.1

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+buildscript {
+    repositories {
+        var bearerToken = System.getenv("LABYMOD_BEARER_TOKEN")
+
+        if (bearerToken == null && project.hasProperty("net.labymod.distributor.bearer-token")) {
+            bearerToken = project.property("net.labymod.distributor.bearer-token").toString()
+        }
+
+        maven("https://dist.labymod.net/api/v1/maven/release/") {
+            name = "LabyMod Distributor"
+
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+
+            credentials(HttpHeaderCredentials::class) {
+                name = "Authorization"
+                value = "Bearer $bearerToken"
+            }
+        }
+
+
+        maven("https://repo.spongepowered.org/repository/maven-public") {
+            name = "SpongePowered Repository"
+        }
+    }
+
+    dependencies {
+        classpath("net.labymod.gradle", "addon", "0.1.1")
+    }
+}
+
+group = "org.example"
+version = "1.0.0"
+
+subprojects {
+    plugins.apply("java-library")
+    plugins.apply("net.labymod.gradle.addon")
+
+    repositories {
+        mavenLocal()
+
+        var bearerToken = System.getenv("LABYMOD_BEARER_TOKEN")
+
+        if (bearerToken == null && project.hasProperty("net.labymod.distributor.bearer-token")) {
+            bearerToken = project.property("net.labymod.distributor.bearer-token").toString()
+        }
+
+        maven("https://dist.labymod.net/api/v1/maven/release/") {
+            name = "LabyMod Distributor"
+
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+
+            credentials(HttpHeaderCredentials::class) {
+                name = "Authorization"
+                value = "Bearer $bearerToken"
+            }
+        }
+
+        maven("https://libraries.minecraft.net/")
+        maven("https://repo.spongepowered.org/repository/maven-public/")
+    }
+}
