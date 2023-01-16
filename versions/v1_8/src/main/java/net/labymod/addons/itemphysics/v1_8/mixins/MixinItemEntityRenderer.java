00b359103fc509b42065b889bde61de9f99f6214
Wazed-Matthis <wazed.wzd@gmail.com>
2023-01-16 17:01:51 +0100

    Fixed 1.8 not working

--- versions/v1_8/src/main/java/net/labymod/addons/itemphysics/v1_8/mixins/MixinItemEntityRenderer.java
+++ versions/v1_8/src/main/java/net/labymod/addons/itemphysics/v1_8/mixins/MixinItemEntityRenderer.java
@@ -21,8 +21,6 @@ import net.labymod.addons.itemphysics.ItemPhysics;
 import net.labymod.addons.itemphysics.ItemPhysicsConfiguration;
 import net.labymod.api.client.render.matrix.Stack;
 import net.labymod.api.inject.LabyGuice;
-import net.labymod.v1_8.client.render.matrix.MatrixStack;
-import net.labymod.v1_8.client.render.matrix.VersionedStackProvider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderHelper;
@@ -37,6 +35,7 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -47,6 +46,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderEntityItem.class)
 public abstract class MixinItemEntityRenderer extends Render<EntityItem> {
+
+  private static final float RAD_TO_DEG = 57.29577951308232F;
 
   private ItemPhysicsConfiguration configuration;
 
@@ -85,8 +86,9 @@ public abstract class MixinItemEntityRenderer extends Render<EntityItem> {
     int seed = Item.getIdFromItem(item) + itemStack.getItemDamage();
     this.field_177079_e.setSeed(seed);
 
+    AxisAlignedBB entityBoundingBox = itemEntity.getEntityBoundingBox();
     float rotation =
-        (((itemEntity.getAge() + partialTicks) / 20.0F + itemEntity.getEyeHeight()) / 10)
+        (((itemEntity.getAge() + partialTicks) / 20.0F + itemEntity.height) / 20)
             * this.configuration.rotationSpeed().get();
 
     this.bindTexture(TextureMap.locationBlocksTexture);
@@ -99,67 +101,66 @@ public abstract class MixinItemEntityRenderer extends Render<EntityItem> {
     RenderHelper.enableStandardItemLighting();
     GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
 
-    Stack stack = Stack.createEmpty();
-    stack.push();
+    GlStateManager.pushMatrix();
 
-    stack.translate((float) x, (float) y, (float) z);
-    stack.scale(0.5f, 0.5f, 0.5f);
+    GlStateManager.translate((float) x, (float) y, (float) z);
+    GlStateManager.scale(0.5f, 0.5f, 0.5f);
 
-    stack.rotateRadians((float) Math.PI / 2, 1, 0, 0);
-    stack.rotateRadians(itemEntity.rotationYaw, 0, 0, 1);
+    GlStateManager.rotate((float) (Math.PI / 2) * RAD_TO_DEG, 1, 0, 0);
+    GlStateManager.rotate(itemEntity.rotationYaw * RAD_TO_DEG, 0, 0, 1);
 
     Minecraft mc = Minecraft.getMinecraft();
     IBakedModel iBakedModel = mc.getRenderItem().getItemModelMesher().getItemModel(itemStack);
 
     this.rotateX(itemEntity, rotation);
     if (iBakedModel.isGui3d()) {
-      stack.translate(0, -0.2f, -0.08f);
+      GlStateManager.translate(0, -0.2f, -0.08f);
     } else if (itemEntity.getEntityWorld().getBlockState(itemEntity.getPosition()).getBlock()
         == Blocks.snow
         || itemEntity.getEntityWorld().getBlockState(itemEntity.getPosition().down()).getBlock()
         == Blocks.soul_sand) {
-      stack.translate(0, 0, -0.14f);
+      GlStateManager.translate(0, 0, -0.14f);
     } else {
-      stack.translate(0, 0, -0.04f);
+      GlStateManager.translate(0, 0, -0.04f);
     }
 
     float height = 0.2f;
     if (iBakedModel.isGui3d()) {
-      stack.translate(0, height, 0);
+      GlStateManager.translate(0, height, 0);
     }
-    stack.rotateRadians(itemEntity.rotationPitch, 0, 1, 0);
+    GlStateManager.rotate(itemEntity.rotationPitch * RAD_TO_DEG, 0, 1, 0);
     if (iBakedModel.isGui3d()) {
-      stack.translate(0, -height, 0);
+      GlStateManager.translate(0, -height, 0);
     }
 
     if (!iBakedModel.isGui3d()) {
       float xO = -0.0F * (renderCount - 1) * 0.5F;
       float yO = -0.0F * (renderCount - 1) * 0.5F;
       float zO = -0.09375F * (renderCount - 1) * 0.5F;
-      stack.translate(xO, yO, zO);
+      GlStateManager.translate(xO, yO, zO);
     }
 
     for (int k = 0; k < renderCount; ++k) {
-      stack.push();
+      GlStateManager.pushMatrix();
       if (k > 0) {
         if (iBakedModel.isGui3d()) {
           float f11 = (this.field_177079_e.nextFloat() * 2.0F - 1.0F) * 0.15F;
           float f13 = (this.field_177079_e.nextFloat() * 2.0F - 1.0F) * 0.15F;
           float f10 = (this.field_177079_e.nextFloat() * 2.0F - 1.0F) * 0.15F;
-          stack.translate(f11, f13, f10);
+          GlStateManager.translate(f11, f13, f10);
         }
       }
 
       iBakedModel.getItemCameraTransforms().applyTransform(TransformType.GROUND);
       this.itemRenderer.renderItem(itemStack, iBakedModel);
 
-      stack.pop();
+      GlStateManager.popMatrix();
       if (!iBakedModel.isGui3d()) {
-        stack.translate(0, 0, 0.09375F);
+        GlStateManager.translate(0, 0, 0.09375F);
       }
     }
 
-    stack.pop();
+    GlStateManager.popMatrix();
 
     GlStateManager.disableRescaleNormal();
     GlStateManager.disableBlend();
@@ -176,4 +177,5 @@ public abstract class MixinItemEntityRenderer extends Render<EntityItem> {
     }
     itemEntity.rotationPitch = itemEntity.rotationPitch + rotation * 2;
   }
+
 }
