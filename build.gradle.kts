37b0b0da1ff4c0eb5a1fd92837c38a1b71efbde8
Nico <jumpingpxl@gmail.com>
2024-11-09 00:57:43 +0100

    bump labygradle to 0.5.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,105 +1,47 @@
 plugins {
-    id("java-library")
-    id("net.labymod.gradle")
-    id("net.labymod.gradle.addon")
+    id("net.labymod.labygradle")
+    id("net.labymod.labygradle.addon")
     id("org.cadixdev.licenser") version ("0.6.1")
 }
 
-group = "org.example"
-version = "1.0.0"
+val versions = providers.gradleProperty("net.labymod.minecraft-versions").get().split(";")
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+group = "net.labymod.addons"
+version = providers.environmentVariable("VERSION").getOrElse("1.0.0")
 
 labyMod {
     defaultPackageName = "net.labymod.addons.itemphysics" //change this to your main package name (used by all modules)
-    addonInfo {
-        namespace = "itemphysics"
-        displayName = "ItemPhysics"
-        author = "CreativeMD (LabyMedia GmbH)"
-        version = System.getenv().getOrDefault("VERSION", "0.0.1")
-    }
 
     minecraft {
-        registerVersions(
-                "1.8.9",
-                "1.12.2",
-                "1.16.5",
-                "1.17.1",
-                "1.18.2",
-                "1.19.2",
-                "1.19.3",
-                "1.19.4",
-                "1.20.1",
-                "1.20.2",
-                "1.20.4",
-                "1.20.5",
-            "1.20.6",
-            "1.21",
-        ) { version, provider ->
-            configureRun(provider, version)
-        }
-
-        subprojects.forEach {
-            if (it.name != "game-runner") {
-                filter(it.name)
+        registerVersion(versions.toTypedArray()) {
+            runs {
+                getByName("client") {
+                    // When the property is set to true, you can log in with a Minecraft account
+                    devLogin = true
+                }
             }
         }
     }
 
-    addonDev {
-        productionRelease()
+    addonInfo {
+        namespace = "itemphysics"
+        displayName = "ItemPhysics"
+        author = "LabyMedia GmbH"
+        minecraftVersion = "*"
+        version = rootProject.version.toString()
     }
 }
 
 subprojects {
-    plugins.apply("java-library")
-    plugins.apply("net.labymod.gradle")
-    plugins.apply("net.labymod.gradle.addon")
+    plugins.apply("net.labymod.labygradle")
+    plugins.apply("net.labymod.labygradle.addon")
     plugins.apply("org.cadixdev.licenser")
 
-    repositories {
-        maven("https://libraries.minecraft.net/")
-        maven("https://repo.spongepowered.org/repository/maven-public/")
-        maven("https://jitpack.io")
-        mavenLocal()
-    }
+    group = rootProject.group
+    version = rootProject.version
 
     license {
         header(rootProject.file("gradle/LICENSE-HEADER.txt"))
         newLine.set(true)
-    }
-}
-
-fun configureRun(provider: net.labymod.gradle.core.minecraft.provider.VersionProvider, gameVersion: String) {
-    provider.runConfiguration {
-        mainClass = "net.minecraft.launchwrapper.Launch"
-        jvmArgs("-Dnet.labymod.running-version=${gameVersion}")
-        jvmArgs("-Dmixin.debug=true")
-        jvmArgs("-Dnet.labymod.debugging.all=true")
-        jvmArgs("-Dmixin.env.disableRefMap=true")
-
-        args("--tweakClass", "net.labymod.core.loader.vanilla.launchwrapper.LabyModLaunchWrapperTweaker")
-        args("--labymod-dev-environment", "true")
-        args("--addon-dev-environment", "true")
-    }
-
-    provider.javaVersion = when (gameVersion) {
-        else -> {
-            JavaVersion.VERSION_21
-        }
-    }
-
-    provider.mixin {
-        val mixinMinVersion = when (gameVersion) {
-            "1.8.9", "1.12.2", "1.16.5" -> {
-                "0.6.6"
-            }
-
-            else -> {
-                "0.8.2"
-            }
-        }
-
-        minVersion = mixinMinVersion
     }
 }
