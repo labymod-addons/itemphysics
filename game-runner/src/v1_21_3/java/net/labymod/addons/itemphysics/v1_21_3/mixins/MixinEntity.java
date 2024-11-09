f17239d34d9dfdc2d870b52b6c233d12ba6c8ecb
Nico <jumpingpxl@gmail.com>
2024-11-09 01:20:53 +0100

    add 1.21.3?

---
+++ game-runner/src/v1_21_3/java/net/labymod/addons/itemphysics/v1_21_3/mixins/MixinEntity.java
@@ -0,0 +1,34 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package net.labymod.addons.itemphysics.v1_21_3.mixins;
+
+import net.labymod.addons.itemphysics.v1_21_3.client.VersionedEntityAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Entity.class)
+public class MixinEntity implements VersionedEntityAccessor {
+
+  @Shadow
+  protected Vec3 stuckSpeedMultiplier;
+
+  public Vec3 getStuckSpeedMultiplier() {
+    return this.stuckSpeedMultiplier;
+  }
+}
---
+++ game-runner/src/v1_21_3/java/net/labymod/addons/itemphysics/v1_21_3/mixins/MixinItemEntityRenderState.java
@@ -0,0 +1,22 @@
+package net.labymod.addons.itemphysics.v1_21_3.mixins;
+
+import net.labymod.addons.itemphysics.v1_21_3.client.VersionedItemEntityRenderStateAccessor;
+import net.minecraft.client.renderer.entity.state.ItemEntityRenderState;
+import net.minecraft.world.entity.item.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ItemEntityRenderState.class)
+public abstract class MixinItemEntityRenderState implements VersionedItemEntityRenderStateAccessor {
+
+  private ItemEntity itemPhysics$itemEntity;
+
+  @Override
+  public ItemEntity getItemEntity() {
+    return this.itemPhysics$itemEntity;
+  }
+
+  @Override
+  public void setItemEntity(ItemEntity itemEntity) {
+    this.itemPhysics$itemEntity = itemEntity;
+  }
+}
