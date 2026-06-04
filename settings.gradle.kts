5c349c5a9b06ea4893af184d2b431d42eece2b3c
Christian <r0bby@gmx.net>
2026-06-04 05:46:50 +0200

    Update LabyGradle to 0.8.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "itemphysics"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.8.0"
+    val labyGradlePluginVersion = "0.8.1"
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
